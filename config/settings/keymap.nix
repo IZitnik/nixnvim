1235bd83ce390beaeac89a2229d518dfd94ca5bf
Ivo Žitník <zitnik.ivo@gmail.com>
2025-08-12 00:28:32 +0200

    Added buffer movement keymaps

--- config/settings/keymap.nix
+++ config/settings/keymap.nix
@@ -7,9 +7,16 @@
     { key = "<leader>fw"; action = "<Cmd>w<Cr>"; }
     { key = "<leader>fq"; action = "<Cmd>q<Cr>"; }
 
+    # pane switching
     { key = "<Left>";  action = "<C-w>h"; }
     { key = "<Down>";  action = "<C-w>j"; }
     { key = "<Up>";    action = "<C-w>k"; }
     { key = "<Right>"; action = "<C-w>l"; }
+
+    # buffer keymaps
+    { key = "<leader><Tab>"; action = "<Cmd>bnext<Cr>"; }
+    { key = "<leader>bb"; action = "<Cmd>bnext<Cr>"; }
+    { key = "<leader>BB"; action = "<Cmd>bNext<Cr>"; }
+    { key = "<leader>bd"; action = "<Cmd>bdelete<Cr>"; }
   ];
 }
